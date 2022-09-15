2f0e96b493ae6f38db92a43ed7739e156649f77c
emmittjames <emmittjames1@gmail.com>
2022-09-14 20:32:14 -0400

    Initial Commit

---
+++ aprizenoonecanwin.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class aprizenoonecanwin{
+
+    private static Scanner sc = new Scanner(System.in);
+    
+    private static int[] getInput(int numOfItems){
+        int[] arr = new int[numOfItems];
+        for(int i=0;i<numOfItems;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+    
+    private static void algorithm(int[] arr,int minCost){
+        int counter = 1;
+        for(int i=1;i<arr.length;i++){
+            if((arr[i]+arr[i-1])<=minCost){
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+
+    public static void main(String[]args){
+        int numOfItems = sc.nextInt();
+        int minCost = sc.nextInt();
+        int[] arr = getInput(numOfItems);
+        algorithm(arr,minCost);
+    }
+}
