8c15bad8b99424b5f145b1c36c95f3e766f0e320
emmittjames <emmittjames1@gmail.com>
2022-10-11 17:49:46 -0400

    Working

---
+++ testdrive.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class testdrive {  
+    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        int third = sc.nextInt();
+        if(((first-second)>0 && (second-third)<0) || ((first-second<0) && (second-third>0))){
+            System.out.println("turned");
+        }
+        else if(Math.abs(third-second) > Math.abs(second-first)){
+            System.out.println("accelerated");
+        }
+        else if(Math.abs(third-second) < Math.abs(second-first)){
+            System.out.println("braked");
+        }
+        else{
+            System.out.println("cruised");
+        }
+    }
+}
